6ed8a79170ac57f93c97a79d81a2d34416b97d01
eduardomantz291 <eduardomantz2112@gmail.com>
2021-01-16 20:09:08 -0300

    logo, input e butão colocado em tela

--- src/pages/Auth/signUp.tsx
+++ src/pages/Auth/signUp.tsx
@@ -1,21 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity, Image, KeyboardAvoidingView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function SignUp() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Worlds</Text>
-      <StatusBar style="inverted"/>
-    </View>
+    <KeyboardAvoidingView style={styles.background}>
+      <View style={styles.containerLogo}>
+        <Image source={require('../../images/logo.png')} />
+      </View>
+
+      <View style={styles.containerInput}>
+        <TextInput style={styles.inputBlock} placeholder="Nome" autoCorrect={false} onChangeText={() => {}}/>
+        <TextInput style={styles.inputBlock} placeholder="Email" autoCorrect={false} onChangeText={() => {}}/>
+        <TextInput style={styles.inputBlock} placeholder="Passoword" autoCorrect={false} onChangeText={() => {}}/>
+        <TextInput style={styles.inputBlock} placeholder="Confirm Password" autoCorrect={false} onChangeText={() => {}}/>
+
+        <TouchableOpacity style={styles.btnSubmit}>
+          <Text style={styles.TextSubmit}>Acessar</Text>
+        </TouchableOpacity>
+        
+        <TouchableOpacity style={styles.btnLogin}>
+          <Text style={styles.TextLogin}>Já tem uma conta</Text>
+        </TouchableOpacity>
+      </View>
+
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#191919'
   },
+
+  containerLogo: {
+
+  },
+
+  containerInput: {
+
+  },
+
+  inputBlock: {
+
+  },
+
+  btnSubmit: {
+
+  },
+  
+  TextSubmit: {
+
+  },
+
+  btnLogin: {
+
+  }, 
+
+  TextLogin: {
+
+  }
 });
