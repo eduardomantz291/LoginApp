e6e8c93463e98539f9994d1cea2434913bf9df32
eduardomantz291 <eduardomantz2112@gmail.com>
2021-01-16 20:43:12 -0300

    layouts da pagina de cadastro pronto

--- src/pages/Auth/signUp.tsx
+++ src/pages/Auth/signUp.tsx
@@ -12,9 +12,10 @@ export default function SignUp() {
       <View style={styles.containerInput}>
         <TextInput style={styles.inputBlock} placeholder="Nome" autoCorrect={false} onChangeText={() => {}}/>
         <TextInput style={styles.inputBlock} placeholder="Email" autoCorrect={false} onChangeText={() => {}}/>
-        <TextInput style={styles.inputBlock} placeholder="Passoword" autoCorrect={false} onChangeText={() => {}}/>
-        <TextInput style={styles.inputBlock} placeholder="Confirm Password" autoCorrect={false} onChangeText={() => {}}/>
-
+        <View style={styles.containerInputPassword}>
+          <TextInput style={styles.inputPassword} placeholder="Passoword" autoCorrect={false} onChangeText={() => {}}/>
+          <TextInput style={styles.inputConfirmPassword} placeholder="Confirm Password" autoCorrect={false} onChangeText={() => {}}/>
+        </View>
         <TouchableOpacity style={styles.btnSubmit}>
           <Text style={styles.TextSubmit}>Acessar</Text>
         </TouchableOpacity>
@@ -37,30 +38,77 @@ const styles = StyleSheet.create({
   },
 
   containerLogo: {
-
+    flex: 1,
+    justifyContent: 'center',
   },
 
   containerInput: {
-
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    paddingBottom: 50,
   },
 
   inputBlock: {
+    backgroundColor: '#fff',
+    width: 350,
+    height: 45,
+    paddingLeft: 10,
+    marginBottom: 15,
+    color: '#222',
+    fontSize: 17,
+    borderRadius: 7
+  },
 
+  containerInputPassword: {
+    flex: 0,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '87%'
+  },
+
+  inputPassword: {
+    backgroundColor: '#fff',
+    width: 160,
+    height: 45,
+    paddingLeft: 10,
+    marginBottom: 15,
+    color: '#222',
+    fontSize: 17,
+    borderRadius: 7
+  },
+
+  inputConfirmPassword: {
+    backgroundColor: '#fff',
+    width: 160,
+    height: 45,
+    paddingLeft: 10,
+    marginBottom: 15,
+    color: '#222',
+    fontSize: 17,
+    borderRadius: 7
   },
 
   btnSubmit: {
-
+    backgroundColor: '#36aaff',
+    width: 350,
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 7,
   },
-  
-  TextSubmit: {
 
+  TextSubmit: {
+    color: '#fff',
+    fontSize: 18
   },
 
   btnLogin: {
-
-  }, 
+    marginTop: 10,
+  },
 
   TextLogin: {
-
+    color: '#fff',
   }
 });
