d25d947d1267a303bbbe48ae0c67486ea2d29b5b
eduardomantz291 <eduardomantz2112@gmail.com>
2021-01-23 21:32:07 -0300

    salvando o usuario logado com AsyncStorage

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 
 interface AuthContextDate {
@@ -6,25 +7,46 @@ interface AuthContextDate {
   user: object | null;
   signIn(userData: object): Promise<void>;
   signOut(): void;
+  loading: boolean;
 }
 
 const AuthContext = createContext<AuthContextDate>({} as AuthContextDate);
 
 export const AuthProvider: React.FC = ({children}) => {
   const [user, setUser] = useState<object | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadStorageData() {
+      const storageUser = await AsyncStorage.getItem('@LAauth:user');
+      const storagerToken = await AsyncStorage.getItem('@LAauth:token');   
+
+      if (storageUser && storagerToken) {
+        setUser(JSON.parse(storageUser));
+        setLoading(false);
+      }
+    }
+
+    loadStorageData();
+  }, [])
 
   async function signIn(userData: object) {
     const response = await api.post("/auth", userData);
     setUser(response.data.user);
     api.defaults.headers.Authorization = `Bearer ${response.data.token}`;
+    
+    await AsyncStorage.setItem('@LAauth:user', JSON.stringify(response.data.user));
+    await AsyncStorage.setItem('@LAauth:token', response.data.token);
   }
 
-  async function signOut() {
-    setUser(null); 
+  function signOut() {
+    AsyncStorage.clear().then(() => {
+      setUser(null);
+    })
   }
 
   return (
-    <AuthContext.Provider value={{ signed: !!user , user, signIn, signOut }}>
+    <AuthContext.Provider value={{ signed: !!user , user, loading, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
